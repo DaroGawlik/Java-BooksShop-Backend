a35358ce79ed14263386fae4be074ee533547ef0
Darek <gawlikdaarek@gmail.com>
2023-08-21 21:59:37 +0200

    docs: added ApplicationUser / Role / UserService

---
+++ src/main/java/com/BooksShopBackend/REST/API/Services/UserService.java
@@ -0,0 +1,34 @@
+package com.BooksShopBackend.REST.API.Services;
+
+import com.BooksShopBackend.REST.API.models.ApplicationUser;
+import com.BooksShopBackend.REST.API.models.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        System.out.println("In the user details service");
+
+        if(!username.equals("Ethan")) throw new UsernameNotFoundException("Not ethan");
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(1, "User"));
+
+        return new ApplicationUser(1, "Ethan", encoder.encode("password"), roles);
+
+    }
+}
