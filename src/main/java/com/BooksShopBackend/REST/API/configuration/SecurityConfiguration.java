b1319d4caa2bc930faaa6fec3fa2ff2d8ab4d411
Darek <gawlikdaarek@gmail.com>
2023-08-24 16:18:06 +0200

    secConf: work with filterChain

--- src/main/java/com/BooksShopBackend/REST/API/configuration/SecurityConfiguration.java
+++ src/main/java/com/BooksShopBackend/REST/API/configuration/SecurityConfiguration.java
@@ -1,23 +1,30 @@
 package com.BooksShopBackend.REST.API.configuration;
 
 
+import com.BooksShopBackend.REST.API.utils.RSAKeyProperties;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtEncoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 //import com.nimbusds.jose.jwk.JWK;
@@ -33,6 +40,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 
 public class SecurityConfiguration {
+
+    private final RSAKeyProperties keys;
+
+    public SecurityConfiguration(RSAKeyProperties keys) {
+        this.keys = keys;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -57,7 +71,34 @@ public class SecurityConfiguration {
                     auth.requestMatchers("/user/**").hasAnyRole("ADMIN", "USER");
                     auth.anyRequest().authenticated();
                 })
+              .oauth2ResourceServer(auth -> auth
+                      .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+              )
+              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
         .httpBasic(withDefaults()).build();
+    }
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        // Tutaj można dodać dodatkowe konfiguracje dla konwertera, jeśli są potrzebne
+        // Tutaj można dodać dodatkowe konfiguracje dla konwertera, jeśli są potrzebne
+
+        // Tutaj można dodać dodatkowe konfiguracje dla konwertera, jeśli są potrzebne
+
+        return converter;
+    }
+
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+
+    @Bean JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
 //    @Bean
--- src/main/java/com/BooksShopBackend/REST/API/utils/KeyGeneratorUtility.java
+++ src/main/java/com/BooksShopBackend/REST/API/utils/KeyGeneratorUtility.java
@@ -1,4 +1,21 @@
 package com.BooksShopBackend.REST.API.utils;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 public class KeyGeneratorUtility {
+
+    public static KeyPair generateRsaKey(){
+
+        KeyPair keyPair;
+
+        try{
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception e){
+            throw new RuntimeException("Error generating RSA key pair", e);
+        }
+        return keyPair;
+    }
 }
